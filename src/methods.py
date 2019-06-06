c7cde317a1e4b2881bf29e9b315519e6e1e76a70
cck_wsl <70700>
2019-06-06 11:21:45 +0800

    initialize

---
+++ src/methods.py
@@ -0,0 +1,25 @@
+import math
+import sys,os
+import numpy as np
+from matplotlib import pyplot as plt
+from collections import defaultdict
+from Bio import Seq, SeqIO, SeqRecord
+
+class DBG():
+    """ The DBG Algorithm to implement the de novo problem. """
+    def __init__(self):
+        print("Initializing...")
+    
+
+    def load_data(self,data_dir='../data/data1',type='short'):
+        filename = 
+
+
+
+
+if __name__=="__main__":
+    dbg = DBG()
+    print("Test Done.")
+    
+    lst = [1,2,3,4]
+    print(lst)
